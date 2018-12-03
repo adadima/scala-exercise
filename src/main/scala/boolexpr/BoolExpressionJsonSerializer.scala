b30a284ceb35cc629a0517f3996b04e018d3a969
Alexandra Dima <adadima@mit.edu>
2018-12-03 10:04:14 -0800

    Added specs for the serializer

--- src/main/scala/boolexpr/BoolExpressionJsonSerializer.scala
+++ src/main/scala/boolexpr/BoolExpressionJsonSerializer.scala
@@ -1,11 +1,18 @@
 package boolexpr
 
-import play.api.libs.json.JsResult.Exception
 import play.api.libs.json._
 
+/**
+  * Json Serializer for boolexpr.BooleanExpression
+  * import: boolexpr.BoolExpressionJsonSerialier._
+  */
 object BoolExpressionJsonSerializer {
 
-    def getJsonObject(expression: BooleanExpression): JsObject = {
+    /* private helper function that defines how to encode case class fields into
+       JsValue objects. This mapping acts as a writer similar to what toJson() method
+       of the play scala framework uses.
+    */
+    private[this] def getJsonObject(expression: BooleanExpression): JsObject = {
         try {
         expression match {
           case True => Json.obj("value" -> JsTrue,
@@ -35,11 +42,28 @@ object BoolExpressionJsonSerializer {
             "type" -> JsString("Not")
           )
         }} catch{
-          case ex: MatchError =>
+          case _: MatchError =>
            throw new IllegalArgumentException(expression + "is innvalid input. Expected type BooleanExpression.")
         }
   }
 
+  /**
+    * Serializes the given boolean expression to json,
+    * specifying for each case class/object the value for
+    * each field taken by constructor as well as the type/case class.
+    *
+    * Example:
+    *
+    *  - True => {\n  "value" : true,\n  "type" : "True"\n}
+    *  - False => {\n  "value" : false,\n  "type" : "False"\n}
+    *  - Variable(string) => {\n  "name" : string,\n  "type" : "Variable"\n}
+    *  - And and Or have similar structure since they are both binary ops =>
+    *  {\n  "leftExpression" : ...,\n  "rightExpression" : ...,\n  "type" : "And"/"Or\n}
+    *  - Not(e) => {\n  "expression" : ...,\n  "type" : "Not"\n}
+    *
+    * @param expression BooleanExpression to serialize
+    * @return a string JSON representation of expression
+    */
     def serialize(expression: BooleanExpression): String = {
        val jsonValue: JsObject = getJsonObject(expression)
        Json.prettyPrint(jsonValue)
